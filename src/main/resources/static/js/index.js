d1796dccd8175fa3ff465ceeef180b967f8ff4d6
MayoneJY <mayone6063@kakao.com>
2024-02-28 11:25:06 +0900

    feat: 커스텀 확장자 추가 버튼 기능 추가.

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,6 +1,8 @@
 const customItems = ["a","b","c","d","chadasdadasdasdasdasdasdasddasdasd","e","f","g","h","i","j","k","l"]
 
-$("#custom-input-text").on("input", function (){
+const $customInputText = $("#custom-input-text");
+
+$customInputText.on("input", function (){
     const $this = $(this);
     const text = $this.text();
     if(text.length > 20){
@@ -29,4 +31,24 @@ customItems.forEach(item => {
 $(".custom-item").on("click", function(){
     $(this).remove();
     customItems.splice(customItems.indexOf($(this).text()), 1);
+})
+
+$("#custom-add").on("click", function(){
+    const text = $customInputText.text();
+    if(text.length > 0){
+        // TODO: 중복된 아이템이 있는지 확인
+        $("#custom-list").append(
+            `<button class="custom-item">
+                ${text}
+                <span class="material-symbols-outlined">
+                    delete
+                </span>
+            </button>`
+        );
+        customItems.push(text);
+        $("#custom-input-text").text("");
+    }
+    else{
+        // TODO: 아무것도 입력하지 않았을 때의 처리
+    }
 })
