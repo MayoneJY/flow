ef128ef46fab0af676b4e41e4a82e891d4a3208e
MayoneJY <mayone6063@kakao.com>
2024-02-29 01:47:04 +0900

    modified: 컨트롤러 파일명 수정.

--- src/main/java/org/mayone/flow/controller/MainController.java
+++ src/main/java/org/mayone/flow/controller/MainController.java
@@ -6,16 +6,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.ui.Model;
 
 @Controller
-public class ThymeleafController {
+public class MainController {
 
     private final ExtensionService extensionService;
 
-    public ThymeleafController(ExtensionService extensionService) {
+    public MainController(ExtensionService extensionService) {
         this.extensionService = extensionService;
     }
 
