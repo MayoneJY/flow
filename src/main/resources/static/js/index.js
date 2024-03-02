995eceb71cf74633a9ab08e35422c90b01dafbb6
MayoneJY <mayone6063@kakao.com>
2024-03-02 15:09:31 +0900

    fix: 한글이 입력이 안 되던 문제 수정.

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -70,7 +70,7 @@ $customInputText.on("input", function (){
     const text = $this.text();
     //[a-zA-Z0-9가-힣]{0,20}
     for(let i = 0; i < text.length; i++){
-        if(!/^[a-zA-Z0-9가-힣]{0,20}$/.test(text[i])){
+        if(!/^[a-zA-Z0-9ㄱ-힣]{0,20}$/.test(text[i])){
             $this.text(text.substring(0, i) + text.substring(i + 1));
             setCursor($this, i);
         }
